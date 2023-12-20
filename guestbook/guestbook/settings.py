99a7a628d7d06c2a9c96dd2ec7180a72e90b893a
mmmlatt <mikael.lattunen@hotmail.fi>
2023-12-20 10:36:07 +0100

    Now able to create app and db with docker-compose

--- guestbook/guestbook/settings.py
+++ guestbook/guestbook/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -88,9 +88,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'guestbookdb',
         'PORT': 5432,
-        'HOST': "f818a9b984b6",
+        'HOST': "guestbook-db-1",
         'USER': "postgres",
-        'PASSWORD': "testpassword",
+        'PASSWORD': os.environ["POSTGRES_PASSWORD"],
     }
 }
 
@@ -130,9 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_DIR = BASE_DIR / 'static'
-STATIC_ROOT = '/static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_DIR = BASE_DIR / 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = 'media/'
